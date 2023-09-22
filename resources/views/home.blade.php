c500caee036383404ffc983327047b77fefb598a
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-22 17:01:30 +0200

    css link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/resources/css/app.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
+
+    <header>
+        <ul>
+            <li><a href="{{'/about'}}">about us</a></li>
+            <li><a href="{{'/contatti'}}">contatti</a></li>
+            <li><a href="{{'/log'}}">log in</a></li>
+            <li><a href="{{'/info'}}">info</a></li>
+        </ul>
+    </header>
     <h1>Welcome to laravel from {{$nome}} {{$cognome}}</h1>
+    
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// route home page 
 Route::get('/', function () {
     $data=[
         "nome"=> "Mariangela",
@@ -20,3 +20,29 @@ Route::get('/', function () {
      ];
     return view('home',$data);
 });
+
+
+// route link about us 
+Route::get('/about', function () {
+  
+    return view('about');
+});
+
+// route link contatti
+Route::get('/contatti', function () {
+  
+    return view('contatti');
+});
+
+// route link log in
+Route::get('/log', function () {
+  
+    return view('log');
+});
+
+// route link info
+Route::get('/info', function () {
+  
+    return view('info');
+});
+
